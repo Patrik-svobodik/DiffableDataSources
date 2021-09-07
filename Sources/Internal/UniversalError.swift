3b49176fc726d1eddfcb380d8966a39263bd3efe
PatrikTheDev <svob.pata@gmail.com>
2021-09-07 17:29:33 +0200

    make universalError public

--- Sources/Internal/UniversalError.swift
+++ Sources/Internal/UniversalError.swift
@@ -1,3 +1,3 @@
-func universalError(_ message: String, file: StaticString = #file, line: UInt = #line) -> Never {
+public func universalError(_ message: String, file: StaticString = #file, line: UInt = #line) -> Never {
     fatalError("[DiffableDataSources] \(message)", file: file, line: line)
 }
